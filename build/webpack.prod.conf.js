f895f69fac8f519a0c537549788d6000af16a550
Claudio Pereira <claudiocleberson@hotmail.com>
2017-11-15 18:49:01 -0200

    Fixed _redirects path

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -101,6 +101,14 @@ var webpackConfig = merge(baseWebpackConfig, {
       {
         from: path.resolve(__dirname, '../static'),
         to: config.build.assetsSubDirectory,
+        ignore: ['.*'],
+        ignore: ['.*','_redirects']
+      }
+    ]),
+    new CopyWebpackPlugin([
+      {
+        from: path.resolve(__dirname, '../static/_redirects'),
+        to: config.build.assetsSubDirectory + '/..',
         ignore: ['.*']
       }
     ])
